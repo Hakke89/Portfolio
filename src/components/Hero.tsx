2a295cf2f91b60aa14bf9a19e66071c9494e2603
Hannes <hannes.vandelaer@vdabcampus.be>
2024-12-17 16:00:33 +0100

    added: hero

---
+++ src/components/Hero.tsx
@@ -0,0 +1,14 @@
+import "./Hero.css";
+
+function Hero(): JSX.Element {
+    return (
+        <div className="hero">
+            <h1>
+                Hello, I'm <span>Hannes</span>.
+            </h1>
+            <h2>I'm a full stack web developer</h2>
+        </div>
+    );
+}
+
+export default Hero;
