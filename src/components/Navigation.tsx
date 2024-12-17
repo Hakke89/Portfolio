743bab4789b639c0e63d328d8063ab59b1a04280
Hannes <hannes.vandelaer@vdabcampus.be>
2024-12-17 16:00:43 +0100

    added: navigation

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,14 @@
+function Navigation(): JSX.Element {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <a href="/about">About</a>
+                    <a href="/projects">Projects</a>
+                </li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navigation;
